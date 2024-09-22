ab194e52d13fcd6fc8375d8d365b4371ce8092f0
EmmanuelBulacio4 <emmanuelbulacio4@gmail.com>
2024-09-22 00:39:27 -0300

    Prep4-nostretch

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,27 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int>numbers = new List<int>();
+        int number = -1;
+        int sum = 0;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+            sum = sum + number;
+        }
+
+        int elements = numbers.Count;
+        float average = ((float) sum) /(elements-1);
+        int maxNumber = numbers.Max();
+
+        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest number is: {maxNumber}");
     }
 }
