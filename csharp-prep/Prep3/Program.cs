1073d625b0632a46314cd4c7393b7a0b47fc4115
EmmanuelBulacio4 <emmanuelbulacio4@gmail.com>
2024-09-21 23:50:58 -0300

    Prep3-strech1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1,101);
+        
+        int counter = 0;
+        int guessedNumber = -1;
+
+        while (guessedNumber != randomNumber)
+        {
+        Console.Write("Guess the number ");
+        guessedNumber = int.Parse(Console.ReadLine());
+        counter ++;
+        if (guessedNumber > randomNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guessedNumber < randomNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else{
+            Console.WriteLine($"You guessed it");
+        }}
     }
 }
