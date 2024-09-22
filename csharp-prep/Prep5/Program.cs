1c55edc256c859ed4f3d69d8e2c125867e6bfa06
EmmanuelBulacio4 <emmanuelbulacio4@gmail.com>
2024-09-22 12:16:22 -0300

    Prep5-nostretch

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        //Mostrar mensaje de bienvenida
+        DisplayMessage();
+
+        //Pedir nombre del usuario y mostrarlo
+        Console.Write("Enter your name: ");
+        string userName = Console.ReadLine();
+        DisplayPersonalMessage(userName);
+
+        //Pedir el numero favorito y mostrarlo
+        Console.Write("Enter you favorite number: ");
+        int favoriteNumber = int.Parse(Console.ReadLine());
+               
+        //Mostrar la raiz cuadrada del numero favorito
+        int square = SquareNumber(favoriteNumber);
+        Console.WriteLine($"{userName}, the square of your number is {square}.");
+        //FUNCIONES
+
+    static void DisplayMessage()
+    {
+    Console.WriteLine("Welcome to the program!");
     }
-}
+
+    static void DisplayPersonalMessage(string userName)
+    {
+    Console.WriteLine($"Hello {userName}");
+    }
+
+    static int SquareNumber (int number)
+    {
+        int square = number*number;
+        return square;
+    }
+
+}//Cierre de la funcion main.
+}//Cierre de Clase
