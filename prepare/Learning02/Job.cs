bf594ca0986f5d23a8e7a1bca3b68e6be306b8b0
EmmanuelBulacio4 <emmanuelbulacio4@gmail.com>
2024-09-24 16:15:41 -0300

    class job file

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,17 @@
+using System.Security.Cryptography.X509Certificates;
+
+public class Job
+{
+    public string _jobTitle = "";
+    public int _startYear;
+    public int _endYear;
+    public string _compani = "";
+
+    public Job()
+    {
+    }
+        public void DisplayJob()
+        {
+            Console.WriteLine($"{_jobTitle} ({_compani}) {_startYear}-{_endYear}");
+        }
+    }
