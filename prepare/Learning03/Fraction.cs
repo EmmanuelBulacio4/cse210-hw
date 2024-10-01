14da01e3d0c950f1d6b89c1e7e3c58859d907d4c
EmmanuelBulacio4 <emmanuelbulacio4@gmail.com>
2024-10-01 18:49:21 -0300

    w3-fraction-finished

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -7,21 +7,54 @@ public class Fraction
     {
         _top = 1;
         _bottom = 1;
-        Console.WriteLine($"{_top}/{_bottom}");
+        //Console.WriteLine($"{_top}/{_bottom}");
+        GetFractionString();
     }
 
     public Fraction(int top)
     {
         _top = top;
         _bottom = 1;
-        Console.WriteLine($"{_top}/{_bottom}");
+        //Console.WriteLine($"{_top}/{_bottom}");
+        GetFractionString();
     }
 
     public Fraction(int top, int bottom)
     {
         _top = top;
         _bottom = bottom;
+        //Console.WriteLine($"{_top}/{_bottom}");
+        GetFractionString();
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+    
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+    
+    public void GetFractionString()
+    {
         Console.WriteLine($"{_top}/{_bottom}");
+    }
+
+    public double GetDecimalValue()
+    {
+        return ((double)_top / (double)_bottom);
     }
 
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,11 +1,21 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
         Fraction f1 = new Fraction();
-        Fraction f2 = new Fraction(6);
-        Fraction f3 = new Fraction(6, 7);
+
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetDecimalValue());
+        
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetDecimalValue());
     }
 }
