8e4fea7a7ef77667e30933917ceb6c9e2f42ec6e
EmmanuelBulacio4 <emmanuelbulacio4@gmail.com>
2024-10-08 20:06:16 -0300

    w3_prove_program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello To Memorize scriptures!!");
+        Scriptures scriptures = new Scriptures("Alma", 36, 24, "Yea, and from that time even until now, I have labored without ceasing, that I might bring souls unto repentance; that I might bring them to taste of the exceeding joy of which I did taste; that they might also be born of God, and be filled with the Holy Ghost.");
+        Console.Clear();
+        scriptures.Show();
     }
 }
