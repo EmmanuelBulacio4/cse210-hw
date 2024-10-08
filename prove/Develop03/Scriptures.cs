64ddc19482f00b4d9739f10e6022630317ea13e5
EmmanuelBulacio4 <emmanuelbulacio4@gmail.com>
2024-10-08 19:20:45 -0300

    classes_w3_prove

--- prove/Develop03/Scriptures.cs
+++ prove/Develop03/Scriptures.cs
@@ -1,8 +1,37 @@
 class Scriptures
 {
-    private string _reference;
+    private Reference _reference;
 
-    // private List<Word> _words;
+    private List<Word> _words;
+
+    public Scriptures(string book, int chapter, int verse, string text)
+    {
+        _reference = new Reference(book, chapter, verse);
+        
+        _words = new List<Word>();
+
+        string [] splitText = text.Split(' ');
+        foreach (string word in splitText)
+        {
+            _words.Add(new Word(word));
+        }
 
 
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Console.WriteLine($"{_reference}:");
+        
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+
+    public bool _isHiden()
+    {
+        return true; //Puse true para despues cambiarlo
+    }
 }
--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,7 +1,30 @@
-class World
+class Word
 {
     private string _text;
     private bool _isHidden;
     
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+    public void Hide()
+    {
+        _isHidden = true;
+    }
 
-}
+    public string Show()
+    {
+        return _text;
+    }
+
+    public bool isHidden()
+    {
+        return _isHidden;
+    }
+
+    public string GetDisplayText()
+    {
+        return ""; //Esto pongo para que no me dé error.
+    }
+    }
