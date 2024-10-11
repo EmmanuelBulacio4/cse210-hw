66fac260125f943a7b094393aa1c141f0e16ad53
EmmanuelBulacio4 <emmanuelbulacio4@gmail.com>
2024-10-11 17:38:46 -0300

    fundation1_finished

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -8,7 +8,7 @@ class Program
         video1._title = "escribir un titulo despues";
         video1._creator = "escribir el autor despues";
         video1._lenght = 205;
-        List<Comment> _video1Comments = new;
+        List<Comment> _video1Comments = new();
         _video1Comments.AddComment("Emma", "That is awesome!!")
         _video1Comments.AddComment("Louis", "great work man!")
         _video1Comments.AddComment("Unknown", "I need to try this!!")
