edaf2e293f54f8d71a82ecf48095bb8db97710c5
EmmanuelBulacio4 <emmanuelbulacio4@gmail.com>
2024-10-15 17:24:51 -0300

    classes_casi-listas

--- prove/Develop05/BreathingActivity.cs
+++ prove/Develop05/BreathingActivity.cs
@@ -6,6 +6,14 @@ public class BreathingActivity : Activity
 
     public void Run()
     {
-        Console.WriteLine();
+        Console.Clear();
+        Console.WriteLine("Wellcome to the Breathing Activity.\n");
+        Console.WriteLine("This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.");
     }
+
+    //Metodos para mostrar mensajes de breath in y bread out
+    //Debe contener una cuenta regresiva
+    //Entre paso y paso debe haber una pausa Thread.Sleep(3000)
+
+    //Mensaje de salida.
 }
--- prove/Develop05/ReflectingActivity.cs
+++ prove/Develop05/ReflectingActivity.cs
@@ -1,5 +1,68 @@
 using System;
 public class ReflectingActivity : Activity
 {
-    
+    private List<string> _prompts;
+    private List<string> _questions;
+
+    ReflectingActivity()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+
+    public string GetRandomPrompt()
+    {
+        List<string> prompts = [
+        "Think of a time when you stood up for someone else.", 
+        "Think of a time when you did something really difficult.",
+        "Think of a time when you helped someone in need.", 
+        "Think of a time when you did something truly selfless."];
+        
+        // Random object
+        Random rnd = new Random();
+
+        // Get random index
+        int index = rnd.Next(prompts.Count);
+
+        // Print random fruit
+        string prompt = prompts[index];
+        return prompt;
+    }
+
+    public string GetRandomQuestion()
+    {
+        List<string> questions = [
+        "Who was the most interesting person I interacted with today?", 
+        "What was the best part of my day?", "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?", 
+        "If I had one thing I could do over today, what would it be?",
+        "What was an important momento today and why?",
+        "Did you help out somebody today?",
+        "What is the most importan lesson you learnd?",
+        "What did you learn today and you like you children remember?"];
+        
+        // Random object
+        Random rnd = new Random();
+
+        // Get random index
+        int index = rnd.Next(questions.Count);
+
+        // Print random fruit
+        string question = questions[index];
+        return question;
+    }
+
+    public void DisplayPrompt()
+    {
+        Console.WriteLine(GetRandomPrompt());
+    }
+
+    public void DisplayQuestions()
+    {
+        Console.WriteLine(GetRandomQuestion());
+    }
 }
