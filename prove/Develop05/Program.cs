bdedd31757fe48e7bc00ce759a5730499db5503c
EmmanuelBulacio4 <emmanuelbulacio4@gmail.com>
2024-10-15 18:05:37 -0300

    program_w5_frame

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
+        bool mainLoop = true;
+        while (mainLoop == true)
+        {
+            Console.WriteLine("Menu Options:\n 1.Start Breathing activit0y\n 2.Star Reflecting activity\n 3.Star Listing activity\n 4.Quit");
+            int election = int.Parse(Console.ReadLine());
+
+            if (election == 1)
+            {
+                Console.WriteLine("Option 1");
+            }
+            else if (election == 2)
+            {
+                Console.WriteLine("Option 2");
+            }
+            else if(election == 3)
+            {
+                Console.WriteLine("Option 3");
+            }
+            else
+            {
+                mainLoop = false;
+            }
+        }
         
     }
-}
+} 
